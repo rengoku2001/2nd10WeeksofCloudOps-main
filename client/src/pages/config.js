7457c335e11247c2aebc5d7c5f9cdf3c9e8b6960
venkatesh <venkateshjatavath2@gmail.com>
2025-05-05 13:53:46 +0530

    modified

--- client/src/pages/config.js
+++ client/src/pages/config.js
@@ -1,8 +1,8 @@
 // const API_BASE_URL = "http://3.84.145.194:84";
-// const API_BASE_URL = "http://veera.narni.co.in";
+// const API_BASE_URL = "http://api.venkcloud.shop ";
 // export default API_BASE_URL;
 // const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://backend";
 // export default API_BASE_URL;
-const API_BASE_URL = "REACT_APP_API_BASE_URL_PLACEHOLDER";
+const API_BASE_URL = "http://api.venkcloud.shop ";
 export default API_BASE_URL;
 
--- terraform_main_ec2/terraform.tf
+++ terraform_main_ec2/terraform.tf
@@ -7,13 +7,13 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "nareshawsdevvvv"
+    bucket = "venkcloudbucket"
     key    = "ec2/terraform.tfstate"
-    region = "us-east-1"
+    region = "ap-south-1"
   }
 
   required_version = ">= 1.6.3"
 }
 provider "aws" {
-  region = "us-east-1"
+  region = "ap-south-1"
 }
