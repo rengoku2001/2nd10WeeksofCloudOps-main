7e43dd1b259257ad310535e59abe392213046ecc
rengoku2001 <venkateshjatavath2@gmail.com>
2025-05-14 17:11:58 +0530

    Update config.js

--- client/src/pages/config.js
+++ client/src/pages/config.js
@@ -1,5 +1,5 @@
 // const API_BASE_URL = "http://3.84.145.194:84";
-const API_BASE_URL = "http://api.venkcloud.shop ";
+const API_BASE_URL = "http://api.venkcloud.shop";
 // export default API_BASE_URL;
 // const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://backend";
 // export default API_BASE_URL;
